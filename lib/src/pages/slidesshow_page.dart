d940c4734942c706606505ec36d1f0a4925466d8
EndersonPro <enjaviga@gmail.com>
2020-05-30 18:11:23 -0500

    creando pagina para mostrar los sliders :ok_hand:

--- lib/src/pages/slidesshow_page.dart
+++ lib/src/pages/slidesshow_page.dart
@@ -3,18 +3,38 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_ui/src/widget/slidesshow.dart';
 
 class SlideshowPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Slideshow(
-        slides: <Widget>[
-          SvgPicture.asset('assets/svgs/slider-1.svg'),
-          SvgPicture.asset('assets/svgs/slider-2.svg'),
-          SvgPicture.asset('assets/svgs/slider-3.svg'),
-          SvgPicture.asset('assets/svgs/slider-4.svg'),
-        ],
-      )
+        // backgroundColor: Colors.red,
+        body: Column(
+      children: <Widget>[
+        Expanded(child: MiSlideShow()),
+        Expanded(child: MiSlideShow())
+      ],
+    ));
+  }
+}
+
+class MiSlideShow extends StatelessWidget {
+  const MiSlideShow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Slideshow(
+      // topDots: true,
+      bulletPrimary: 12.0,
+      bulletSeconday: 9,
+      // colorPrimaryDots: Colors.red,
+      // colorSecundaryDots: Colors.white,
+      slides: <Widget>[
+        SvgPicture.asset('assets/svgs/slider-1.svg'),
+        SvgPicture.asset('assets/svgs/slider-2.svg'),
+        SvgPicture.asset('assets/svgs/slider-3.svg'),
+        SvgPicture.asset('assets/svgs/slider-4.svg'),
+      ],
     );
   }
 }
